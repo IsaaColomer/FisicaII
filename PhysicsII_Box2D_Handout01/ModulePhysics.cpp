36fad42c4c6cf4d4ce3c7d0485b3dd31768f614c
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2020-10-06 11:39:19 +0200

    Added gravity

--- PhysicsII_Box2D_Handout01/ModulePhysics.cpp
+++ PhysicsII_Box2D_Handout01/ModulePhysics.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 #include "ModulePhysics.h"
 #include "math.h"
+#include "ModuleInput.h"
 
 // TODO 1: Include Box 2 header and library
 
@@ -37,6 +38,12 @@ bool ModulePhysics::Start()
 	b2FixtureDef fixture;
 	fixture.shape = &shape;
 	body->CreateFixture(&fixture);
+
+	
+	
+	
+
+
 	return true;
 }
 
@@ -53,11 +60,30 @@ update_status ModulePhysics::PostUpdate()
 {
 	// TODO 5: On space bar press, create a circle on mouse position
 	// - You need to transform the position / radius
+
 	if(App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
 		debug = !debug;
 
 	if(!debug)
 		return UPDATE_CONTINUE;
+
+
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
+	{
+		b2BodyDef body_Mov;
+		body_Mov.type = b2_dynamicBody;
+		body_Mov.position.Set(PIXELS_TO_METERS(App->input->GetMouseX()), PIXELS_TO_METERS(App->input->GetMouseY()));
+		body_Mov.gravityScale = -10.0f;
+		body_Mov.active = true;
+
+		b2Body* bodyMov = ModulePhysics::world->CreateBody(&body_Mov);
+
+		b2CircleShape shape2;
+		shape2.m_radius = PIXELS_TO_METERS(50);
+		b2FixtureDef fixture2;
+		fixture2.shape = &shape2;
+		bodyMov->CreateFixture(&fixture2);
+	}
 
 	// Bonus code: this will iterate all objects in the world and draw the circles
 	// You need to provide your own macro to translate meters to pixels
@@ -72,6 +98,8 @@ update_status ModulePhysics::PostUpdate()
 					b2CircleShape* shape = (b2CircleShape*)f->GetShape();
 					b2Vec2 pos = f->GetBody()->GetPosition();
 					App->renderer->DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), METERS_TO_PIXELS(shape->m_radius), 255, 255, 255);
+
+					b2CircleShape* shape2 = (b2CircleShape*)f->GetShape();
 				}
 				break;
 
